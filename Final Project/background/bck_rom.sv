fbe892f0f5d263966588245d2f26821e4c2deae8
Ralph Nathan <77542739+TacTissue@users.noreply.github.com>
2023-12-07 01:06:56 -0600

    Files Backgrnd

---
+++ Final Project/background/bck_rom.sv
@@ -0,0 +1,13 @@
+module bck_rom (
+	input logic clock,
+	input logic [18:0] address,
+	output logic [4:0] q
+);
+
+logic [4:0] memory [0:307199] /* synthesis ram_init_file = "./bck/bck.COE" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
