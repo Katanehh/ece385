b5d94ee7b051db18d850ce40afd7ab4212644bf4
Ralph Nathan <77542739+TacTissue@users.noreply.github.com>
2023-12-07 07:24:21 -0600

    w

---
+++ Final Project/Number0/Number0_rom.sv
@@ -0,0 +1,13 @@
+module Number0_rom (
+	input logic clock,
+	input logic [11:0] address,
+	output logic [1:0] q
+);
+
+logic [1:0] memory [0:2499] /* synthesis ram_init_file = "./Number0/Number0.COE" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
