d7858cde6f6f0d9d107604d22f7522812c02fe92
Ralph Nathan <77542739+TacTissue@users.noreply.github.com>
2023-12-07 07:27:02 -0600

    ZUTOFIA

---
+++ Final Project/Number9/Number9_rom.sv
@@ -0,0 +1,13 @@
+module Number9_rom (
+	input logic clock,
+	input logic [11:0] address,
+	output logic [1:0] q
+);
+
+logic [1:0] memory [0:2499] /* synthesis ram_init_file = "./Number9/Number9.COE" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
