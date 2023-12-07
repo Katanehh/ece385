1e2878432cc94011be8e1809de6fcf55762be89a
Ralph Nathan <77542739+TacTissue@users.noreply.github.com>
2023-12-07 06:55:25 -0600

    mimz

---
+++ Final Project/pass/pass_rom.sv
@@ -0,0 +1,13 @@
+module pass_rom (
+	input logic clock,
+	input logic [15:0] address,
+	output logic [1:0] q
+);
+
+logic [1:0] memory [0:44999] /* synthesis ram_init_file = "./pass/pass.COE" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
